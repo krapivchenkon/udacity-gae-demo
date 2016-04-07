740721375309e633ec74499788ca318c37426ed9
krapivchenkon <krapivchenkon@gmail.com>
2016-04-07 10:20:54 +0300

    Some datastore advance queries added

--- conference_central/settings.py
+++ conference_central/settings.py
@@ -13,4 +13,5 @@ created/forked from conference.py by wesc on 2014 may 24
 # Replace the following lines with client IDs obtained from the APIs
 # Console or Cloud Console.
 WEB_CLIENT_ID = '418530274270-80rpci6i3b6c2b5j3rtusue65457i22v.apps.googleusercontent.com'
+# WEB_CLIENT_ID = 'test'
 
